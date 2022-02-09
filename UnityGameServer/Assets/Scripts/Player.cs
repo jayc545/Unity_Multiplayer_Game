25868e64e4942767101e1a95d62ea52e96544d4c
jayc545 <54627710+jayc545@users.noreply.github.com>
2022-02-09 22:52:09 +0100

    More progressssss

--- UnityGameServer/Assets/Scripts/Player.cs
+++ UnityGameServer/Assets/Scripts/Player.cs
@@ -98,7 +98,7 @@ public class Player : MonoBehaviour
         {
             if (_hit.collider.CompareTag("Player"))
             {
-
+                _hit.collider.GetComponent<Player>().TakeDamage(50f);
             }
         }
     }
@@ -117,6 +117,7 @@ public class Player : MonoBehaviour
             controller.enabled = false;
             transform.position = new Vector3(0f, 25f, 0f);
             ServerSend.PlayerPosition(this);
+            StartCoroutine(Respawn());
         }
 
         ServerSend.PlayerHealth(this);
